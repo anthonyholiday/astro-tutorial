5fc7294c1b62e2eab9e74928514b3b0e71bdc2ca
Anthony Holiday <138096306+anthonyholiday@users.noreply.github.com>
2026-05-31 09:20:35 +0100

    Adding Astro Islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [preact()],
   adapter: vercel(),
   site: "https://astro-tutorial-nine-henna.vercel.app"
 });
